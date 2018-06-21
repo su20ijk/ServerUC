624e27afebb47ef7c4a28447c1c313d6d1748f53
Gene Lee <leexgenex@gmail.com>
2018-06-21 15:17:17 -0500

    celsius to fahrenheit converter (works)

---
+++ ServerUC/Controllers/TempController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerUC.Controllers
+{
+    public class TempController : Controller
+    {
+        public ActionResult CelsiusToFahrenheit()
+        {
+            return View(viewName: "CelsiusToFahrenheit", model: 0.0);
+        }
+
+        [HttpPost]
+        public ActionResult CtoF(double inputC)
+        {
+            return View(viewName: "CelsiusToFahrenheit", model: inputC * (9 / 5) + 32);
+        }
+    }
+}
