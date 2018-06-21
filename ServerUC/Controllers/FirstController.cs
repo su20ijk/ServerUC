24b04b9ecc06f97bff3535e575b80269d60ad16e
su20 <19liuq@elmbrookstudents.org>
2018-06-21 14:58:24 -0500

    WORKED!!!

---
+++ ServerUC/Controllers/FirstController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerUC.Controllers
+{
+    public class FirstController : Controller
+    {
+        public ActionResult BaseConversion()
+        {
+            return View(viewName: "BaseConversion", model: " ");
+        }
+        [HttpPost]
+        public ActionResult BaseConversion(int startNumber)
+        {
+            return View("BaseConversion", model: "Your number will be: " + Convert.ToString(startNumber, 2));
+        }
+    }
+}
