189c5769ab2147513d180c63dc16ea7031250789
Andrew Kim <19kima@elmbrookstudents.org>
2018-06-21 14:58:39 -0500

    created and made it work

---
+++ ServerUC/Controllers/KimController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace ServerUC.Controllers
+{
+    public class KimController : Controller
+    {
+        public ActionResult CmToM()
+        {
+            return View(viewName: "CmtoM", model: 0.0);
+        }
+
+        [HttpPost]
+        public ActionResult convert(double centimeter)
+        {
+            return View(viewName: "CmtoM", model: centimeter/100);
+        }
+    }
+}
